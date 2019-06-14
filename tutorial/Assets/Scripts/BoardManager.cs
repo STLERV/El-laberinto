577bf038da32add826c4586583453c17d98c21b6
Adria <adri.navarrete.09@gmail.com>
2019-06-14 12:15:27 +0200

    version 2

--- tutorial/Assets/Scripts/BoardManager.cs
+++ tutorial/Assets/Scripts/BoardManager.cs
@@ -25,8 +25,8 @@ public class BoardManager : MonoBehaviour
     }
 
 
-    public int columns = 8;                                         //Number of columns in our game board.
-    public int rows = 11;                                            //Number of rows in our game board.
+    public int columns = 15;                                         //Number of columns in our game board.
+    public int rows = 15;                                            //Number of rows in our game board.
     public Count wallCount = new Count(5, 9);                      //Lower and upper limit for our random number of walls per level.
     public Count foodCount = new Count(1, 5);                      //Lower and upper limit for our random number of food items per level.
     public GameObject exit;                                         //Prefab to spawn for exit.
@@ -42,14 +42,12 @@ public class BoardManager : MonoBehaviour
 
 
 
-    string mapa = "n,n,n,n,n,n,n,n,n,n" +
-                  "n,n,n,n,n,n,n,n,n,n" +
-                  "n,n,n,n,n,n,n,n,n,n" +
-                  "n,n,n,n,n,n,n,n,n,n" +
-                  "n,n,n,n,n,P,n,n,n,n" +
-                  "n,n,n,n,n,n,n,n,n,n" +
-                  "n,n,n,n,n,n,n,n,n,n" +
-                  "n,n,n,n,n,n,n,n,n,n";
+
+
+
+
+
+
     private object instance;
 
     void InitialiseList()
@@ -70,14 +68,21 @@ public class BoardManager : MonoBehaviour
 
     public void BoardSetup()
     {
-        string mapa = "n,P,n,n,n,n,n,n,n,n" +
-                      "n,n,n,n,n,n,n,n,n,n" +
-                      "n,n,n,n,n,n,n,n,n,n" +
-                      "n,n,n,n,n,n,n,n,n,n" +
-                      "n,n,n,n,n,n,n,n,n,n" +
-                      "n,n,n,n,n,n,n,n,n,n" +
-                      "n,n,n,n,n,n,C,C,n,n" +
-                      "n,n,n,n,n,n,n,n,n,n";
+        string mapa = "n,n,M,M,M,M,M,M,M,M,M,M,M,M,M," +
+                      "n,n,n,n,n,n,n,n,n,n,n,n,n,n,M," +
+                      "M,n,n,n,n,n,n,n,n,n,E,n,n,n,M," +
+                      "M,n,n,n,C,n,n,n,n,n,n,n,n,n,M," +
+                      "M,n,n,n,n,n,n,n,n,n,n,n,n,n,M," +
+                      "M,n,n,n,n,n,n,n,n,n,E,n,n,n,M," +
+                      "M,n,n,n,n,n,n,n,n,n,n,n,n,n,M," +
+                      "M,n,n,n,n,n,n,C,n,n,n,n,n,n,M," +
+                      "M,n,n,n,n,n,n,n,n,n,n,n,n,n,M," +
+                      "M,n,n,n,n,n,n,n,n,n,n,n,n,n,M," +
+                      "M,n,n,n,n,n,n,n,n,n,n,n,n,n,M," +
+                      "M,n,n,n,n,n,n,n,n,n,n,n,n,n,M," +
+                      "M,n,n,n,n,n,n,n,n,n,n,n,n,n,M," +
+                      "M,n,n,n,n,n,n,n,n,n,n,n,n,n,M," +
+                      "M,M,M,M,M,M,M,M,M,M,M,M,M,M,M";
 
 
         boardHolder = new GameObject("Board").transform;
@@ -87,132 +92,129 @@ public class BoardManager : MonoBehaviour
 
 
 
-        for (int x = -1; x < columns + 1; x++)
+        for (int y = 0; y < columns; y++)
         {
+            
 
-            for (int y = -1; y < rows + 1; y++)
+            for (int x = 0; x < rows; x++)
             {
-
+                Debug.Log("i=" + i);
                 if (mapa1[i] == "n")
                 {
                     toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
                     pintar(toInstantiate, x, y);
                 }
-                if (mapa1[i] == "P")
+                else if (mapa1[i] == "P")
                 {
+                    toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                    pintar(toInstantiate, x, y);
                     Instantiate(exit, new Vector3(x, y, 0f), Quaternion.identity);
                 }
-                if (mapa1[i] == "C")
+                else if (mapa1[i] == "C")
                 {
+                    toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                    pintar(toInstantiate, x, y);
                     toInstantiate = foodTiles[Random.Range(0, foodTiles.Length)];
                     pintar(toInstantiate, x, y);
                 }
-                if (mapa1[i] == "E")
+                else if (mapa1[i] == "E")
                 {
+                    toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                    pintar(toInstantiate, x, y);
                     toInstantiate = enemyTiles[Random.Range(0, enemyTiles.Length)];
                     pintar(toInstantiate, x, y);
-                    if (mapa1[i] == "M")
-                    {
-                        toInstantiate = enemyTiles[Random.Range(0, enemyTiles.Length)];
-                        pintar(toInstantiate, x, y);
-                    }
-
-
-                    if (x == -1 || x == columns || y == -1 || y == rows) //para muros
-                    {
-                        toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
-                        pintar(toInstantiate, x, y);
-                    }
-
-
-
                 }
-
-
+                else
+                {
+                    
+                    toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+                    pintar(toInstantiate, x, y);
+                }
+                
+                i++;
             }
-
-
         }
     }
 
-         public void pintar(GameObject toInstantiate, int x, int y) {
+    public void pintar(GameObject toInstantiate, int x, int y)
+    {
 
-             GameObject instacia;
+        GameObject instacia;
 
-          instacia = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
-         instacia.transform.SetParent(boardHolder);
-        }
-
-
-
+        instacia = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+        instacia.transform.SetParent(boardHolder);
+    }
 
 
-        //Sets up the outer walls and floor (background) of the game board.
-        void BoardSetup2()
+
+
+
+    //Sets up the outer walls and floor (background) of the game board.
+    void BoardSetup2()
+    {
+        //Instantiate Board and set boardHolder to its transform.
+        boardHolder = new GameObject("Board").transform;
+
+        //Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
+        for (int x = -1; x < columns + 1; x++)
         {
-            //Instantiate Board and set boardHolder to its transform.
-            boardHolder = new GameObject("Board").transform;
-
-            //Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
-            for (int x = -1; x < columns + 1; x++)
+            //Loop along y axis, starting from -1 to place floor or outerwall tiles.
+            for (int y = -1; y < rows + 1; y++)
             {
-                //Loop along y axis, starting from -1 to place floor or outerwall tiles.
-                for (int y = -1; y < rows + 1; y++)
-                {
-                    //Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
-                    GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                //Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
+                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
 
-                    //Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
-                    if (x == -1 || x == columns || y == -1 || y == rows)
-                        toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+                //Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                    toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
 
-                    //Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
-                    GameObject instance =
-                        Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                //Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+                GameObject instance =
+                    Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
 
-                    //Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
-                    instance.transform.SetParent(boardHolder);
-                }
+                //Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+                instance.transform.SetParent(boardHolder);
             }
         }
+    }
 
 
-        //RandomPosition returns a random position from our list gridPositions.
-        Vector3 RandomPosition()
+    //RandomPosition returns a random position from our list gridPositions.
+    Vector3 RandomPosition()
+    {
+
+        int randomIndex = Random.Range(0, gridPositions.Count);
+
+
+        Vector3 randomPosition = gridPositions[randomIndex];
+
+
+        gridPositions.RemoveAt(randomIndex);
+
+
+        return randomPosition;
+    }
+
+
+    //LayoutObjectAtRandom accepts an array of game objects to choose from along with a minimum and maximum range for the number of objects to create.
+    void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+    {
+        //Choose a random number of objects to instantiate within the minimum and maximum limits
+        int objectCount = Random.Range(minimum, maximum + 1);
+
+        //Instantiate objects until the randomly chosen limit objectCount is reached
+        for (int i = 0; i < objectCount; i++)
         {
-          
-            int randomIndex = Random.Range(0, gridPositions.Count);
+            //Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
+            Vector3 randomPosition = RandomPosition();
 
-           
-            Vector3 randomPosition = gridPositions[randomIndex];
+            //Choose a random tile from tileArray and assign it to tileChoice
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
 
-            
-            gridPositions.RemoveAt(randomIndex);
-
-            
-            return randomPosition;
+            //Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
+            Instantiate(tileChoice, randomPosition, Quaternion.identity);
         }
-
-
-        //LayoutObjectAtRandom accepts an array of game objects to choose from along with a minimum and maximum range for the number of objects to create.
-        void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
-        {
-            //Choose a random number of objects to instantiate within the minimum and maximum limits
-            int objectCount = Random.Range(minimum, maximum + 1);
-
-            //Instantiate objects until the randomly chosen limit objectCount is reached
-            for (int i = 0; i < objectCount; i++)
-            {
-                //Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
-                Vector3 randomPosition = RandomPosition();
-
-                //Choose a random tile from tileArray and assign it to tileChoice
-                GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
-
-                //Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
-                Instantiate(tileChoice, randomPosition, Quaternion.identity);
-            }
-        }
+    }
 
 
 
@@ -221,29 +223,29 @@ public class BoardManager : MonoBehaviour
 
 
 
-        //SetupScene initializes our level and calls the previous functions to lay out the game board
-        public void SetupScene(int level)
-        {
+    //SetupScene initializes our level and calls the previous functions to lay out the game board
+    public void SetupScene(int level)
+    {
 
-            BoardSetup();
-
-
-            InitialiseList();
+        BoardSetup();
 
 
-            LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
+        InitialiseList();
 
 
-            LayoutObjectAtRandom (foodTiles,2, 2);
+        //LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
 
 
-            int enemyCount = (int)Mathf.Log(level, 2f);
-
-              LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
+        //LayoutObjectAtRandom (foodTiles,2, 2);
 
 
-             Instantiate(exit, new Vector3 (columns - 1, rows - 1, 0f), Quaternion.identity);
-        }
+        int enemyCount = (int)Mathf.Log(level, 2f);
+
+        //LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
+
+
+        Instantiate(exit, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
+    }
 }
 
 
@@ -384,4 +386,4 @@ public class NewBehaviourScript : MonoBehaviour
             Instantiate (exit, new Vector3 (columns - 1, rows - 1, 0f), Quaternion.identity);
         }
     }
-*/ 
+*/
